a38f42a80f05100ddc74e72df010a8ada034edb1
Christopher Toth <q.alpha@gmail.com>
2022-03-05 15:02:28 -0700

    Updated package structure to conform to modern Python standards

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup
 
 setup(
  name = "paypal_IPN",
- version = 0.15,
+ version = "0.16",
+ author = "Christopher Toth",
+ author_email = "q@q-continuum.net",
  description = "Simple PayPal IPN Listener",
- package_dir = {'paypal_IPN': '.'},
  packages = ['paypal_IPN'],
  install_requires = ['requests'],
 )
